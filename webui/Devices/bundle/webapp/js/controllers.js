75cef656d4a150d42ca337abbc27d2bf45cd73e4
Guenter Obiltschnig <guenter.obiltschnig@appinf.com>
2015-02-23 11:06:51 +0100

    support for table sorting

--- webui/Devices/bundle/webapp/js/controllers.js
+++ webui/Devices/bundle/webapp/js/controllers.js
@@ -5,6 +5,10 @@ var devicesControllers = angular.module('devicesControllers', []);
 devicesControllers.controller('DevicesCtrl', ['$scope', '$http', '$interval',
   function ($scope, $http, $interval) {
     $scope.devices = [];
+    $scope.orderBy = "name";
+    $scope.setOrderBy = function(col) {
+      $scope.orderBy = col;
+    }
     $http.get('/macchina/devices/devices.jss').success(function(data) {
       $scope.devices = data;
     });
