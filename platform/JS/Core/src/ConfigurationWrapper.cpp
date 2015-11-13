5d02e9962107966eb087c78a244ab5185ded8439
Guenter Obiltschnig <guenter.obiltschnig@appinf.com>
2015-11-13 23:46:47 +0100

    add getObject() method to Configuration object to parse JSON config values

--- platform/JS/Core/src/ConfigurationWrapper.cpp
+++ platform/JS/Core/src/ConfigurationWrapper.cpp
@@ -42,6 +42,7 @@ v8::Handle<v8::ObjectTemplate> ConfigurationWrapper::objectTemplate(v8::Isolate*
 	configurationTemplate->Set(v8::String::NewFromUtf8(pIsolate, "getDouble"), v8::FunctionTemplate::New(pIsolate, getDouble));
 	configurationTemplate->Set(v8::String::NewFromUtf8(pIsolate, "getBool"), v8::FunctionTemplate::New(pIsolate, getBool));
 	configurationTemplate->Set(v8::String::NewFromUtf8(pIsolate, "getString"), v8::FunctionTemplate::New(pIsolate, getString));
+	configurationTemplate->Set(v8::String::NewFromUtf8(pIsolate, "getObject"), v8::FunctionTemplate::New(pIsolate, getObject));
 	configurationTemplate->Set(v8::String::NewFromUtf8(pIsolate, "has"), v8::FunctionTemplate::New(pIsolate, has));
 	configurationTemplate->Set(v8::String::NewFromUtf8(pIsolate, "set"), v8::FunctionTemplate::New(pIsolate, set));
 	configurationTemplate->Set(v8::String::NewFromUtf8(pIsolate, "keys"), v8::FunctionTemplate::New(pIsolate, keys));
@@ -145,6 +146,36 @@ void ConfigurationWrapper::getString(const v8::FunctionCallbackInfo<v8::Value>& 
 }
 
 
+void ConfigurationWrapper::getObject(const v8::FunctionCallbackInfo<v8::Value>& args)
+{
+	if (args.Length() < 1) return;
+	v8::HandleScope scope(args.GetIsolate());
+	Poco::Util::AbstractConfiguration* pConfig = Wrapper::unwrapNative<Poco::Util::AbstractConfiguration>(args);
+	std::string key = toString(args[0]);
+	try
+	{
+		if (args.Length() > 1 && !pConfig->has(key))
+		{
+			args.GetReturnValue().Set(args[1]);
+		}
+		else 
+		{
+			std::string json = pConfig->getString(key);
+			v8::Local<v8::String> jsonString = v8::String::NewFromUtf8(args.GetIsolate(), json.c_str());
+			v8::Local<v8::Value> value = v8::JSON::Parse(jsonString);
+			if (!value.IsEmpty())
+				args.GetReturnValue().Set(value);
+			else
+				throw Poco::DataFormatException("Invalid JSON", json);
+		}
+	}
+	catch (Poco::Exception& exc)
+	{
+		returnException(args, exc);
+	}
+}
+
+
 void ConfigurationWrapper::has(const v8::FunctionCallbackInfo<v8::Value>& args)
 {
 	if (args.Length() < 1) return;
@@ -168,7 +199,15 @@ void ConfigurationWrapper::set(const v8::FunctionCallbackInfo<v8::Value>& args)
 	v8::HandleScope scope(args.GetIsolate());
 	Poco::Util::AbstractConfiguration* pConfig = Wrapper::unwrapNative<Poco::Util::AbstractConfiguration>(args);
 	std::string key = toString(args[0]);
-	std::string value = toString(args[1]);
+
+	// use built-in JSON.stringify() 
+	v8::Local<v8::Context> context = args.GetIsolate()->GetCurrentContext();
+	v8::Local<v8::Object> global = context->Global();
+	v8::Local<v8::Object> json = global->Get(v8::String::NewFromUtf8(args.GetIsolate(), "JSON"))->ToObject();
+	v8::Local<v8::Function> stringify = v8::Handle<v8::Function>::Cast(json->Get(v8::String::NewFromUtf8(args.GetIsolate(), "stringify")));
+	v8::Local<v8::Value> argv[1] = {args[1]};
+	std::string value = toString(stringify->Call(json, 1, argv));
+
 	try
 	{
 		pConfig->setString(key, value);
