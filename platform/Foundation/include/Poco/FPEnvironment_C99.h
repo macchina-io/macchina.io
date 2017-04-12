f2857627929be78fad20a618bd2216e0e1a46883
Guenter Obiltschnig <guenter.obiltschnig@appinf.com>
2017-04-12 18:10:18 +0200

    added basic support for QNX

--- platform/Foundation/include/Poco/FPEnvironment_C99.h
+++ platform/Foundation/include/Poco/FPEnvironment_C99.h
@@ -75,48 +75,56 @@ private:
 //
 inline bool FPEnvironmentImpl::isInfiniteImpl(float value)
 {
-	return std::isinf(value) != 0;
+	using namespace std;
+	return isinf(value) != 0;
 }
 
 
 inline bool FPEnvironmentImpl::isInfiniteImpl(double value)
 {
-	return std::isinf(value) != 0;
+	using namespace std;
+	return isinf(value) != 0;
 }
 
 
 inline bool FPEnvironmentImpl::isInfiniteImpl(long double value)
 {
-	return std::isinf((double) value) != 0;
+	using namespace std;
+	return isinf((double) value) != 0;
 }
 
 
 inline bool FPEnvironmentImpl::isNaNImpl(float value)
 {
-	return std::isnan(value) != 0;
+	using namespace std;
+	return isnan(value) != 0;
 }
 
 
 inline bool FPEnvironmentImpl::isNaNImpl(double value)
 {
-	return std::isnan(value) != 0;
+	using namespace std;
+	return isnan(value) != 0;
 }
 
 
 inline bool FPEnvironmentImpl::isNaNImpl(long double value)
 {
-	return std::isnan((double) value) != 0;
+	using namespace std;
+	return isnan((double) value) != 0;
 }
 
 
 inline float FPEnvironmentImpl::copySignImpl(float target, float source)
 {
+	using namespace std;
 	return copysignf(target, source);
 }
 
 
 inline double FPEnvironmentImpl::copySignImpl(double target, double source)
 {
+	using namespace std;
 	return copysign(target, source);
 }
 
