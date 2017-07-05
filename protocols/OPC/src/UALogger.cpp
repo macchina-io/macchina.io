0b8ea5d0f82044d2bc79c65592b77f867502f649
Alex Fabijanic <alex@pocoproject.org>
2017-07-05 12:32:07 +0200

    replace spaces with tabs

--- protocols/OPC/src/UALogger.cpp
+++ protocols/OPC/src/UALogger.cpp
@@ -23,48 +23,48 @@ Poco::Logger* uaLogger = nullptr;
 
 void UA_Log_POCO(UA_LogLevel level, UA_LogCategory category, const char *msg, ...)
 {
-    using namespace IoT::OPC;
+	using namespace IoT::OPC;
 
-    va_list args; va_start(args, msg);
-    char c[2] = {0};
-    int count = vsnprintf(c, 1, msg, args);
-    va_end(args);
-    if(count < 0)
-    {
-        fprintf(stderr, "%s", strerror(errno));
-        fflush(stderr);
-    }
-    else
-    {
-        char* buf = (char*) calloc(count + 1, 1);
-        if(uaLogger && buf)
-        {
-            va_list args; va_start(args, msg);
-            vsnprintf(buf, count + 1, msg, args);
-            //printf("logging=%d, %p, %d, %p, %s\n", count, uaLogger, category, buf, buf);
-            switch(category)
-            {
-                case UA_LOGLEVEL_TRACE: 
-                    uaLogger->trace(buf);
-                    break;
-                case UA_LOGLEVEL_DEBUG:
-                    uaLogger->debug(buf);
-                    break;
-                case UA_LOGLEVEL_INFO:
-                    uaLogger->information(buf);
-                    break;
-                case UA_LOGLEVEL_WARNING:
-                    uaLogger->warning(buf);
-                    break;
-                case UA_LOGLEVEL_ERROR:
-                    uaLogger->error(buf);
-                    break;
-                case UA_LOGLEVEL_FATAL:
-                    uaLogger->fatal(buf);
-                    break;
-            }
-            va_end(args);
-        }
-        free(buf);
-    }
+	va_list args; va_start(args, msg);
+	char c[2] = {0};
+	int count = vsnprintf(c, 1, msg, args);
+	va_end(args);
+	if(count < 0)
+	{
+		fprintf(stderr, "%s", strerror(errno));
+		fflush(stderr);
+	}
+	else
+	{
+		char* buf = (char*) calloc(count + 1, 1);
+		if(uaLogger && buf)
+		{
+			va_list args; va_start(args, msg);
+			vsnprintf(buf, count + 1, msg, args);
+			//printf("logging=%d, %p, %d, %p, %s\n", count, uaLogger, category, buf, buf);
+			switch(category)
+			{
+				case UA_LOGLEVEL_TRACE: 
+					uaLogger->trace(buf);
+					break;
+				case UA_LOGLEVEL_DEBUG:
+					uaLogger->debug(buf);
+					break;
+				case UA_LOGLEVEL_INFO:
+					uaLogger->information(buf);
+					break;
+				case UA_LOGLEVEL_WARNING:
+					uaLogger->warning(buf);
+					break;
+				case UA_LOGLEVEL_ERROR:
+					uaLogger->error(buf);
+					break;
+				case UA_LOGLEVEL_FATAL:
+					uaLogger->fatal(buf);
+					break;
+			}
+			va_end(args);
+		}
+		free(buf);
+	}
 }
