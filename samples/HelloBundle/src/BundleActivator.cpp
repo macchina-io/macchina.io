d67f1c0c7f073ec664fa3fdf6f23b99a0a9d93a0
Günter Obiltschnig <guenter.obiltschnig@appinf.com>
2018-02-20 15:29:41 +0100

    added sample bundle

---
+++ samples/HelloBundle/src/BundleActivator.cpp
@@ -0,0 +1,39 @@
+//
+// BundleActivator.cpp
+//
+// Copyright (c) 2018, Applied Informatics Software Engineering GmbH.
+// All rights reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+
+#include "Poco/OSP/BundleActivator.h"
+#include "Poco/OSP/BundleContext.h"
+#include "Poco/ClassLibrary.h"
+
+
+namespace HelloBundle {
+
+
+class BundleActivator: public Poco::OSP::BundleActivator
+{
+public:
+	void start(Poco::OSP::BundleContext::Ptr pContext)
+	{
+		pContext->logger().information("Hello, world!");
+	}
+
+	void stop(Poco::OSP::BundleContext::Ptr pContext)
+	{
+		pContext->logger().information("Goodbye!");
+	}
+};
+
+
+} // namespace HelloBundle
+
+
+POCO_BEGIN_MANIFEST(Poco::OSP::BundleActivator)
+	POCO_EXPORT_CLASS(HelloBundle::BundleActivator)
+POCO_END_MANIFEST
