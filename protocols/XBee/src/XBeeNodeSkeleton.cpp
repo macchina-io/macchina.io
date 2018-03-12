c39bba94919dff047e2b3913b3533113428ccea2
Günter Obiltschnig <guenter.obiltschnig@appinf.com>
2018-03-12 07:14:22 +0100

    regenerated Remoting files

--- protocols/XBee/src/XBeeNodeSkeleton.cpp
+++ protocols/XBee/src/XBeeNodeSkeleton.cpp
@@ -30,6 +30,7 @@
 #include "IoT/XBee/ZigBeeTransmitRequestSerializer.h"
 #include "Poco/RemotingNG/Deserializer.h"
 #include "Poco/RemotingNG/MethodHandler.h"
+#include "Poco/RemotingNG/RemotingException.h"
 #include "Poco/RemotingNG/Serializer.h"
 #include "Poco/RemotingNG/ServerTransport.h"
 #include "Poco/RemotingNG/TypeDeserializer.h"
