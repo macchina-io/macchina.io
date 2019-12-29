41ec7d4e4462a646a775681da7a1f4fade9ffb60
Günter Obiltschnig <guenter.obiltschnig@appinf.com>
2019-12-29 09:00:25 +0100

    add codeCache to LD_LIBRARY_PATH

--- env.bash
+++ env.bash
@@ -14,16 +14,19 @@ osarch=`uname -m`
 export MACCHINA_BASE="$basedir"
 export MACCHINA_VERSION=`cat $basedir/VERSION`
 export PROJECT_BASE="$MACCHINA_BASE"
+export MACCHINA_CODECACHE="$MACCHINA_BASE/server/bin/$osname/$osarch/codeCache"
 export POCO_BASE="$MACCHINA_BASE/platform"
 if [ $osname = "Darwin" ] ; then
-	export DYLD_LIBRARY_PATH=$POCO_BASE/lib/$osname/$osarch:$DYLD_LIBRARY_PATH
+	export DYLD_LIBRARY_PATH=$MACCHINA_CODECACHE:$POCO_BASE/lib/$osname/$osarch:$DYLD_LIBRARY_PATH
 	libPath=$DYLD_LIBRARY_PATH
 	libPathVar="DYLD_LIBRARY_PATH"
 else
-	export LD_LIBRARY_PATH=$POCO_BASE/lib/$osname/$osarch:$LD_LIBRARY_PATH
+	export LD_LIBRARY_PATH=$MACCHINA_CODECACHE:$POCO_BASE/lib/$osname/$osarch:$LD_LIBRARY_PATH
 	libPath=$LD_LIBRARY_PATH
 	libPathVar="LD_LIBRARY_PATH  "
 fi
+
+mkdir -p $MACCHINA_CODECACHE
 
 echo "macchina.io build environment set."
 echo ""
